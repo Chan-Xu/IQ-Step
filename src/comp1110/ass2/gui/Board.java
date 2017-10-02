5d0defc5c53467b8e09c382ab4d01b74afb19b61
Yiwen Peng <u6071714@anu.edu.au>
2017-10-03 00:53:15 +1100

    task7 the board show

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -1,12 +1,13 @@
 package comp1110.ass2.gui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.Group;
 public class Board extends Application {
     private static final int BOARD_WIDTH = 933;
     private static final int BOARD_HEIGHT = 700;
-
+   private Group root =new Group();
     // FIXME Task 7: Implement a basic playable Steps Game in JavaFX that only allows pieces to be placed in valid places
 
 
@@ -18,6 +19,9 @@ public class Board extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+    primaryStage.setTitle("IQ game");
+    Scene scene= new Scene(root,BOARD_WIDTH,BOARD_WIDTH);
+    primaryStage.setScene(scene);
+    primaryStage.show();
     }
 }
