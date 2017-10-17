7e96ebce252ed89a1a05f0b5fd70a93c2d6fe860
Yiwen Peng <u6071714@anu.edu.au>
2017-10-17 23:54:50 +1100

    to get the char after rotate

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -1,42 +1,52 @@
 package comp1110.ass2.gui;
-import comp1110.ass2.StepsGame;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
+
+
 import javafx.stage.Stage;
 import javafx.scene.Group;
-import javafx.scene.Cursor;
+
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.Node;
 
-import javax.xml.parsers.SAXParser;
-import java.awt.*;
-import java.nio.file.attribute.GroupPrincipal;
+
+
+
+
+
+
+
+
+
 
 public class Board extends Application {
     // the canves size
     private static final int BOARD_WIDTH = 1200;
     private static final int BOARD_HEIGHT = 900;
+    // Piece
+    private static final int Piece_Size=150;
     // the board
     private static final int START_X=50;
     private static final int START_Y=200;
-    private static final int SPACE=50;// equall to 2pacing
     private static final int r =12;
-    private static  final int WIDTH =(SPACE/2+r+((2*r)+SPACE)*5)*(16/11);
-    private static final int HEIGHT=(START_Y+(SPACE/2+r)*4+r)-START_Y+2*r+SPACE;
+    private static  final int WIDTH = Piece_Size*4;
+    private static final int Space=Piece_Size/4;
+    private static final int PieceR=Piece_Size/3;
     private static final String URI_BASE = "assets/";
-// Piece
-    private static final int Piece_Size=150;
+
     private final Group root = new Group();
+    //to save the flip and rotation
+    char[]flipRot = new char[8];
+    // save the postion
+    char[] pos=new char[8];
     // FIXME Task 7: Implement a basic playable Steps Game in JavaFX that only allows pieces to be placed in valid places
     class Piece extends ImageView{
         int originx,originy;
         char piece,flip;
+        int index;
                 Piece(char piece, char flip){
                     if(!(piece>='A'&&piece<='H')){
                         throw new IllegalArgumentException("no this picture");
@@ -44,6 +54,7 @@ public class Board extends Application {
                         setImage(new Image((getClass().getResource(URI_BASE+piece+flip+".png").toString())));
                         this.piece = piece;
                         this.flip = flip;
+                        index=piece-'A';
                     }
                     else { throw new IllegalArgumentException("no this picture");
                     }
@@ -66,7 +77,10 @@ public class Board extends Application {
         private double mouseY ;
         public DraggablePiece(char piece,char flip) {
             super(piece,flip);
-           setOnScroll(event->rotate());
+           setOnScroll(event->{
+               rotate();
+ System.out.println(""+getFlipRot());
+           });
             setOnMousePressed(event -> {
                 mouseX = event.getSceneX() ;
                 mouseY = event.getSceneY() ;
@@ -82,24 +96,26 @@ public class Board extends Application {
             });
             setOnMouseReleased(event -> {
                 if(onBoard()){
-//                    if((""+this.piece+this.flip).equals("AA")){// CHANGE THIS CONDITION
+                    if((""+piece+flipRot[index]).equals("AB")){// CHANGE THIS CONDITION
                         snapToGrid();
-//                    }else{
-//                        snapToHome();
-//                    }
+                    }else{
+                        snapToHome();
+                    }
                 }else{
                     snapToHome();
                 }
             });
 // ??? how to change this if click this remove the old one???
             setOnMouseClicked(event ->{
-//                root.getChildren().add(new DraggablePiece(this.piece,'E')));
-//                root.getChildren().remove(DraggablePiece(this.piece, `this.flip)));
+                System.out.println(""+this.flip+this.piece);
+//                root.getChildren().add(new DraggablePiece(this.piece,'E'));
+//                root.getChildren().remove(new DraggablePiece(this.piece, this.flip));
+
             });
         }
         private boolean onBoard(){
-            return getLayoutX()>=(START_X-50)&&getLayoutX()<=(START_X+SPACE/2+r+((2*r)+SPACE)*4+r)-20
-                    &&getLayoutY()>=(START_Y-50)&&getLayoutY()<=(START_Y+(SPACE/2+r)*4+r);
+            return getLayoutX()>=(START_X-50)&&getLayoutX()<=(START_X+WIDTH+Space-Piece_Size)
+                    &&getLayoutY()>=(START_Y-50)&&getLayoutY()<=(START_Y+(Space*5+r)-Piece_Size);
         }
         private void snapToHome(){
             setLayoutX(originx);
@@ -110,17 +126,23 @@ public class Board extends Application {
             double centreX,centreY;
             centreX=getLayoutX()+Piece_Size/2;
             centreY=getLayoutY()+Piece_Size/2;
-            translateX= (centreX-START_X+WIDTH/24)/(WIDTH/16);
-            translateY= (centreY-START_Y+WIDTH/24)/(WIDTH/16);
-            setLayoutX((int)translateX*(WIDTH/16)+START_X-Piece_Size/2-r);
-            setLayoutY((int)translateY*(WIDTH/16)+START_Y-Piece_Size/2-r);
+            translateX= (centreX-START_X+PieceR/2)/(Space);
+            translateY= (centreY-START_Y+PieceR/2)/(Space);
+            setLayoutX((int)translateX*(Space)+START_X-Piece_Size/2);
+            setLayoutY((int)translateY*(Space)+START_Y-Piece_Size/2);
         }
 
         private void rotate(){
             setRotate((getRotate()+90)%360);
-        }
+                    }
         // to flip the piece
+          private char getFlipRot(){
+            int rotate=(int)getRotate()/90;
+            char val=(char) (flip+rotate);
 
+           return flipRot[index]=val;
+
+          }
     }
 //    private void flipPiece(){
 //        DraggablePiece a=new DraggablePiece('A','A');
@@ -133,11 +155,11 @@ public class Board extends Application {
             for (int col=0;col<5;col++){
                 double x;
                 if (row%2==0){
-                    x=START_X+((2*r)+SPACE)*col;
+                    x=START_X+Space*2*col;
                 }else{
-                    x =START_X+SPACE/2+r+((2*r)+SPACE)*col;
+                    x =START_X+Space+Space*2*col;
                 }
-                double y =START_Y+(SPACE/2+r)*row;
+                double y =START_Y+Space*row;
                 Circle circle = new Circle(x, y, r);
                 circle.setFill(Color.GRAY);
                 root.getChildren().add(circle);
